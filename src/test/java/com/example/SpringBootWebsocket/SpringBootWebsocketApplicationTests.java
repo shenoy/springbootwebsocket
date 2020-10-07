7e05e3ea3efc197f88d7797385fb958041a94f5c
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-10-07 17:05:57 +0100

    initial commit

---
+++ src/test/java/com/example/SpringBootWebsocket/SpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
